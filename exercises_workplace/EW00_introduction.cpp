f0f2d0a7ccba5a45c9f4aaac8a143a3067091b0a
Gabriel Cristian Circiu <gaci@itu.dk>
2026-09-01 21:30:26 +0200

    feat: More CMake fix and EW00 extra stuff
    Some extra problems came up so Gemini made additional changes to CMakeLists, this makes it possible to finally build everything without errors. As we have not learned about it more in depth, I assume this is okay.
    
    Otherwise, I have done some of the extra steps for the exercise and have a somewhat working final result but with some minor bugs... I will need to ask about it, very interesting behavior!
    
    Lastly I allowed the exercises workspace to be committed to the repo since this is a fork.

---
+++ exercises_workplace/EW00_introduction.cpp
@@ -0,0 +1,338 @@
+#include "SDL3/SDL_keycode.h"
+#include "SDL3/SDL_mouse.h"
+#include <SDL3/SDL.h>
+
+int main(void) {
+
+  // toggle to swith between the insulated player update (aka, the way you want
+  // to do it) and the one performed immediate after polling the event queue
+  bool use_insulated_player_update = true;
+
+  float window_w = 800;
+  float window_h = 600;
+  int target_framerate_ms = 1000 / 60;       // 16 milliseconds
+  int target_framerate_ns = 1000000000 / 60; // 16666666 nanoseconds
+
+  SDL_Window *window =
+      SDL_CreateWindow("ES00 - introduction (solved)", window_w, window_h, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, "vulkan");
+
+  // increase the zoom to make debug text more legible
+  // (ie, on the class projector, we will usually use 2)
+  {
+    float zoom = 1;
+    window_w /= zoom;
+    window_h /= zoom;
+    SDL_SetRenderScale(renderer, zoom, zoom);
+  }
+
+  bool quit = false;
+
+  SDL_Time walltime_frame_beg = 0;
+  SDL_Time walltime_work_end = 0;
+  SDL_Time walltime_frame_end = 0;
+  SDL_Time time_elapsed_frame = 0;
+  SDL_Time time_elapsed_work = 0;
+
+  SDL_Time time_elapsed_sleep;
+  SDL_Time time_elapsed_busywait;
+
+  int delay_type = 0;
+
+  float player_speed = 2;
+  float player_size = 40;
+  SDL_FRect player_rect;
+  player_rect.w = player_size;
+  player_rect.h = player_size;
+  player_rect.x = window_w / 2 - player_size;
+  player_rect.y = window_h / 2 - player_size / 2;
+
+  SDL_FRect player_two_rect;
+  player_two_rect.w = player_size;
+  player_two_rect.h = player_size;
+  player_two_rect.x = window_w / 2 + player_size;
+  player_two_rect.y = window_h / 2 - player_size / 2;
+
+  SDL_FRect npc_rect;
+  npc_rect.w = player_size;
+  npc_rect.h = player_size;
+  npc_rect.x = window_w / 4;
+  npc_rect.y = window_h / 4;
+
+  // NOTE: list of stuff with the same prefix? Looks like it's a good candidate
+  // for consolidation
+  bool btn_pressed_up = false;
+  bool btn_pressed_down = false;
+  bool btn_pressed_left = false;
+  bool btn_pressed_right = false;
+
+  bool btn_p2_pressed_up = false;
+  bool btn_p2_pressed_down = false;
+  bool btn_p2_pressed_left = false;
+  bool btn_p2_pressed_right = false;
+
+  // Mouse stats
+  float mouse_x = 0.0f;
+  float mouse_y = 0.0f;
+  bool mouse_button_L = false;
+  bool mouse_button_R = false;
+  bool mouse_button_M = false;
+
+  SDL_GetCurrentTime(&walltime_frame_beg);
+  while (!quit) {
+    // input
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_EVENT_QUIT:
+        quit = true;
+        break;
+
+      // NOTE: when there is no break, both switch cases will execute the same
+      // code
+      //       block. These kind of "clever" solutions can become messy very
+      //       fast. We will soon move it to a more appropriate function (with a
+      //       more solid event parsing).
+      case SDL_EVENT_KEY_UP:
+      case SDL_EVENT_KEY_DOWN: {
+        // player inputs
+        // NOTE: OS and hardware will notify events at their own pace,
+        // re-triggering
+        //       events and other shenanigans. We want to insulate our game code
+        //       from this, so here we will just keep track of events happening
+        //       and do ACTUAL updates later in the loop
+        if (use_insulated_player_update) {
+          // insulated movement: store the fact that an event happened, so we
+          // can use it at the appropriate time during update
+          if (event.key.key == SDLK_W)
+            btn_pressed_up = event.key.down;
+          if (event.key.key == SDLK_S)
+            btn_pressed_down = event.key.down;
+          if (event.key.key == SDLK_A)
+            btn_pressed_left = event.key.down;
+          if (event.key.key == SDLK_D)
+            btn_pressed_right = event.key.down;
+          if (event.key.key == SDLK_UP)
+            btn_p2_pressed_up = event.key.down;
+          if (event.key.key == SDLK_DOWN)
+            btn_p2_pressed_down = event.key.down;
+          if (event.key.key == SDLK_LEFT)
+            btn_p2_pressed_left = event.key.down;
+          if (event.key.key == SDLK_RIGHT)
+            btn_p2_pressed_right = event.key.down;
+        } else {
+          // raw movement: compute player position immediately.
+          // THIS IS NOT HOW YOU WANT TO DO IT! Everything regarding the game
+          // should be decoupled from external events
+          if (event.key.key == SDLK_W)
+            player_rect.y -= player_speed;
+          if (event.key.key == SDLK_S)
+            player_rect.y += player_speed;
+          if (event.key.key == SDLK_A)
+            player_rect.x -= player_speed;
+          if (event.key.key == SDLK_D)
+            player_rect.x += player_speed;
+        }
+
+        // debug utilities
+        if (event.key.down) {
+          // common trick to convert a key range to an array inde index
+          // (works because if we look up the values of `SDLK_1` & co. we see
+          // that they are all contiguous!)
+          if (event.key.key >= SDLK_1 && event.key.key < SDLK_6)
+            delay_type = event.key.key - SDLK_1;
+          if (event.key.key == SDLK_F1)
+            use_insulated_player_update = !use_insulated_player_update;
+        }
+        break;
+      }
+
+      case SDL_EVENT_MOUSE_BUTTON_UP:
+      case SDL_EVENT_MOUSE_BUTTON_DOWN: {
+        if (event.button.button == SDL_BUTTON_LEFT)
+          mouse_button_L = event.button.down;
+        if (event.button.button == SDL_BUTTON_MIDDLE)
+          mouse_button_M = event.button.down;
+        if (event.button.button == SDL_BUTTON_RIGHT)
+          mouse_button_R = event.button.down;
+        break;
+      }
+      }
+    }
+
+    SDL_MouseButtonFlags buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
+
+    // clear screen
+    // NOTE: `0x` prefix means we are expressing the number in hexadecimal (base
+    // 16)
+    //       `0b` is another useful prefix, expresses the number in binary
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+
+    // SDL_Delay(500);
+
+    if (use_insulated_player_update) {
+      // update player position, but ensure not going off screen
+      if ((btn_pressed_up) && (player_rect.y > 0))
+        player_rect.y -= player_speed;
+      if ((btn_pressed_down) && (player_rect.y + player_rect.h < window_h))
+        player_rect.y += player_speed;
+      if ((btn_pressed_left) && (player_rect.x > 0))
+        player_rect.x -= player_speed;
+      if ((btn_pressed_right) && (player_rect.x + player_rect.w < window_w))
+        player_rect.x += player_speed;
+      // update player two position, but ensure not going off screen
+      if ((btn_p2_pressed_up) && (player_two_rect.y > 0))
+        player_two_rect.y -= player_speed;
+      if ((btn_p2_pressed_down) &&
+          (player_two_rect.y + player_two_rect.h < window_h))
+        player_two_rect.y += player_speed;
+      if ((btn_p2_pressed_left) && (player_two_rect.x > 0))
+        player_two_rect.x -= player_speed;
+      if ((btn_p2_pressed_right) &&
+          (player_two_rect.x + player_two_rect.w < window_w))
+        player_two_rect.x += player_speed;
+    }
+
+    // NPC Behaviour
+    // Choose a random direction, then move the npc that way
+    // Why does it always go down right???
+    switch (SDL_rand(4)) {
+    case 0:
+      npc_rect.y -= player_speed;
+    case 1:
+      npc_rect.y += player_speed;
+    case 2:
+      npc_rect.x -= player_speed;
+    case 3:
+      npc_rect.x += player_speed;
+    }
+
+    // Implement hit mechanics somewhere
+
+    SDL_SetRenderDrawColor(renderer, 0x3C, 0x63, 0xFF, 0XFF);
+    SDL_RenderFillRect(renderer, &player_rect);
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x63, 0x3C, 0XFF);
+    SDL_RenderFillRect(renderer, &player_two_rect);
+    SDL_SetRenderDrawColor(renderer, 0x3C, 0xFF, 0x3C, 0XFF);
+    SDL_RenderFillRect(renderer, &npc_rect);
+
+    SDL_GetCurrentTime(&walltime_work_end);
+    time_elapsed_work = walltime_work_end - walltime_frame_beg;
+    walltime_frame_end = walltime_work_end;
+
+    if (target_framerate_ns > time_elapsed_work) {
+      switch (delay_type) {
+      case 0: {
+        // busy wait - very precise, but costly
+        while (walltime_frame_end - walltime_frame_beg < target_framerate_ns)
+          SDL_GetCurrentTime(&walltime_frame_end);
+
+        time_elapsed_busywait = walltime_frame_end - walltime_work_end;
+        time_elapsed_sleep = 0;
+        break;
+      }
+      case 1: {
+        // simple delay - too imprecise
+        // NOTE: `SDL_Delay` gets milliseconds, but our timer gives us
+        // nanoseconds! We need to covert it manually
+        SDL_Delay((target_framerate_ns - time_elapsed_work) / 1000000);
+        SDL_GetCurrentTime(&walltime_frame_end);
+
+        time_elapsed_busywait = 0;
+        time_elapsed_sleep = walltime_frame_end - walltime_frame_beg;
+        break;
+      }
+      case 2: {
+        // delay ns - also too imprecise
+        SDL_DelayNS(target_framerate_ns - time_elapsed_work);
+        SDL_GetCurrentTime(&walltime_frame_end);
+
+        time_elapsed_busywait = 0;
+        time_elapsed_sleep = walltime_frame_end - walltime_frame_beg;
+        break;
+      }
+      case 3: {
+        // delay precise
+        SDL_DelayPrecise(target_framerate_ns - time_elapsed_work);
+        SDL_GetCurrentTime(&walltime_frame_end);
+
+        time_elapsed_busywait = 0;
+        time_elapsed_sleep = walltime_frame_end - walltime_frame_beg;
+        break;
+      }
+      case 4: {
+        // custom delay - we use the sleeping delay with an arbitrary margin,
+        // then we busywait what's left
+        const Uint64 sleep_safety_margin = 1000000; // ie, 1ms
+        SDL_Time walltime_sleep_end;
+
+        SDL_DelayNS(target_framerate_ns - time_elapsed_work -
+                    sleep_safety_margin);
+        SDL_GetCurrentTime(&walltime_sleep_end);
+        walltime_frame_end = walltime_sleep_end;
+
+        while (walltime_frame_end - walltime_frame_beg < target_framerate_ns)
+          SDL_GetCurrentTime(&walltime_frame_end);
+
+        time_elapsed_busywait = walltime_frame_end - walltime_sleep_end;
+        time_elapsed_sleep = walltime_sleep_end - walltime_work_end;
+        break;
+      }
+      }
+    }
+
+    time_elapsed_frame = walltime_frame_end - walltime_frame_beg;
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderDebugTextFormat(renderer, 10.0f, 10.0f,
+                              "elapsed (frame): %9.6f ms",
+                              (float)time_elapsed_frame / (float)1000000);
+    SDL_RenderDebugTextFormat(renderer, 10.0f, 20.0f,
+                              "elapsed (work) : %9.6f ms",
+                              (float)time_elapsed_work / (float)1000000);
+    SDL_RenderDebugTextFormat(renderer, 10.0f, 30.0f,
+                              "delay type: %d (change with 1-5)",
+                              delay_type + 1);
+
+    SDL_RenderDebugTextFormat(renderer, 10.0f, 50.0f,
+                              "time spent sleeping   : %9.6f ms",
+                              (float)time_elapsed_sleep / (float)1000000);
+    SDL_RenderDebugTextFormat(renderer, 10.0f, 60.0f,
+                              "time spent busywaiting: %9.6f ms",
+                              (float)time_elapsed_busywait / (float)1000000);
+
+    SDL_RenderDebugTextFormat(
+        renderer, 10.0f, 80.0f, "update type (toggle with F1) : %9s",
+        use_insulated_player_update ? "INSULATED" : "IMMEDIATE");
+
+    SDL_RenderDebugTextFormat(renderer, 10.0f, 100.0f, "Mouse position %5d %5d",
+                              (int)mouse_x, (int)mouse_y);
+    SDL_RenderDebugTextFormat(
+        renderer, 10.0f, 110.0f, "Mouse buttons: %s %s %s",
+        mouse_button_L ? "L" : "-", mouse_button_M ? "M" : "-",
+        mouse_button_R ? "R" : "-");
+
+    // render
+    SDL_RenderPresent(renderer);
+
+    // NOTE: while taking the time two different times is no ideal, in the
+    // current setup we have a problem:
+    //       our `time_elapsed_frame` doesn't take into account the time it
+    //       takes to render the debug view, AND to "present" the graphics.
+    //       Usually that is not a big deal, but if if the untimed stuff takes
+    //       too long we end up in a death spiral! Our options are:
+    //       - take the time again after everything has been done (which means
+    //       the time logged will be slightly lower)
+    //       - show the elapsed time from the previous frame
+    //       we will go with the first option here
+    // walltime_frame_beg = walltime_frame_end;
+    SDL_GetCurrentTime(&walltime_frame_beg);
+  }
+
+  // NOTE: we created a bunch of resources (window, renderer). Should we
+  // explicitely destroy them here?
+  //       it's actually not a trivial question!
+
+  return 0;
+};
